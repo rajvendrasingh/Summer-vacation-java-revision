c7042bfedec2b48942d2350f8e33f876e58d424e
rajvendrasingh <sseema7350@gmail.com>
2026-06-04 21:21:24 +0530

    Add files via upload

---
+++ Day1HomeWork.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+public class Day1HomeWork {
+   /* public static int avg( int a, int b , int c){
+        return(a+b+c)/3;
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int Avg =avg(a,b,c);
+        System.out.println("Average of three numbers: " + Avg);
+    }*/
+   /*public static int SumofOddNum(int n){
+    int sum = 0;
+    for (int i = 1; i <= n; i++){
+        if (i % 2 != 0){
+            sum += i;
+        }
+    }
+    return sum;
+   }
+
+   public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int Sum = SumofOddNum(n);
+        System.out.println(Sum);
+        sc.close();
+        
+   }*/
+  /*public static double circumference(double r){
+    return 2 * Math.PI * r;
+  }
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    double r = sc.nextDouble();
+    double Circumference = circumference(r);
+    System.out.println("Circumference of circle: " + Circumference);
+    sc.close();
+  }*/
+
+    /*public static String age(int year){
+        if (year>18){
+            return "elegible for vote";
+        }
+        else{
+            return "not elegnle for vote";
+        }
+    }
+    public static void main(String[]args){
+        Scanner  sc = new Scanner (System.in);
+        int year = sc.nextInt();
+        String Age =age(year);
+        System.out.println(Age);
+          sc.close();
+    }*/
+
+          // do while llopp
+          /*public static void main(String[]args){
+            do{
+                System.out.println("hello");
+            }while(true);
+          }
+   */
+
+
+
+}
