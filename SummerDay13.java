8770dd1143d7050df00d1b22b294434ec930ff09
rajvendrasingh <sseema7350@gmail.com>
2026-06-17 17:18:37 +0530

    Add files via upload

---
+++ SummerDay13.java
@@ -0,0 +1,93 @@
+public class SummerDay13 {
+    //   recursion Day-3
+    // print fact of a number
+
+
+    // public static void main(String[] args) {
+    //     int n=5;
+    //     int fact=1;
+    
+    //     for(int i = 1 ; i<=5; i++){
+    //         fact*=i;
+    //         System.out.println(fact);
+            
+    //     }
+        
+//    public static int fact(int n){
+//    if(n==1 || n==0){
+//     return 1;
+//    }
+
+//     int fact_nm1=fact(n-1);
+//     int fac_n=n*fact_nm1;
+//     return fac_n;
+//    }
+
+//    public static void main(String[] args) {
+//       int n=5;
+//      int fact= fact(n);
+//      System.out.println(fact);
+//    }
+
+   // print fibonacci 
+//    public static void main(String[] args) {
+    
+   
+//    int n = 10;
+//    int a = 0;
+//    int b = 1;
+//    for ( int i = 1; i <=n ; i++){
+//     System.out.println(a+ " ");
+//     int c= a+b;
+//     a=b;
+//     b=c;
+//    }
+
+//    } 
+
+// public static void printfib(int a , int b, int n){
+//     if(n==0){
+//         return;
+//     }
+//    int c = a+b;
+//    System.out.println(c);
+//   printfib(b,c,n-1);
+
+
+// }
+// public static void main(String[] args) {
+//      int a=0, b=1;
+//      System.out.println(a);
+//      System.out.println(b);
+//      int n = 7;
+//      printfib(a, b, n-2);
+// }
+
+//print x^n(stack height =n)
+
+// public static void main (String []arg){
+//  int a=2;
+//  int n=5;
+//  int result=1;
+//  for (int i= 1; i <n; i++){
+//      result =a*result;
+//     System.out.println(result);
+
+//  }
+//}
+
+// public static int printxn(int a ,int n ,int result){
+//     if(n==0){
+//         return result;
+//     }
+
+//     return printxn(a,n-1,result*a);
+// }
+// public static void main(String[] args) {
+//     int a = 2;
+//     int n=5;
+//     int result = 1;
+//    int print = printxn(a,n,result);
+//    System.out.println(print);
+// }
+} 
