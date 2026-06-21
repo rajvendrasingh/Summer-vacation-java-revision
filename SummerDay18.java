90491324c649b31d3d214994c90ae611e62d5334
rajvendrasingh <sseema7350@gmail.com>
2026-06-21 19:39:30 +0530

    Add files via upload

---
+++ SummerDay18.java
@@ -0,0 +1,26 @@
+public class SummerDay18 {
+    // find the 1st &last occurance of an element in string
+//     public static int first =-1;
+//     public static int last=-1;
+
+//     public static void findoccur(String str, int idx, char element){
+//         if(idx==str.length()){
+//              System.out.println(first);
+//              System.out.println(last);
+//              return;
+//         }
+//         char currentchar =str.charAt(idx);
+//         if(currentchar==element){
+//             if(first==-1){
+//                 first=idx;
+//             }
+//             // update last every time we see the element
+//             last = idx;
+//         }
+//         findoccur(str,idx+1,element);
+//     }
+//     public static void main(String[] args) {
+//         String str= "ababahgas";
+//         findoccur(str, 0, 'a');
+//     }
+// }
