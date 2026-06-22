66a5745a7535e8f5fd2aa4820f79591bba0b1a62
rajvendrasingh <sseema7350@gmail.com>
2026-06-22 13:47:50 +0530

    Add files via upload

---
+++ SummerDay19.java
@@ -0,0 +1,44 @@
+public class SummerDay19 {
+    
+    // sorted array{1,2,3,4,}
+    // public static void sortedarr(int[] arr,int idx){
+    //   if(idx==arr.length-1){
+    //     System.out.println("arr sorted");
+    //     return;
+    //   }
+       
+    
+    //     if(arr[idx]>arr[idx+1]){
+    //         System.out.println("arr is not sorted");
+    //         return ;
+    //     }
+    //      sortedarr(arr, idx+1);
+        
+    // }
+    // public static void main(String[] args) {
+    //     int arr[]={1,2,3,6,5};
+       
+    //     sortedarr(arr, 0);
+
+        
+
+    // }
+
+    // public static boolean sortarr(int[] arr, int idx){
+    //     if(idx==arr.length-1){
+    //         return true;
+    //     }
+    //     if(arr[idx]>=arr[idx+1]){
+    //         return false;
+    //     }
+    //     return sortarr(arr, idx+1);
+    // }
+    // public static void main(String[]arg){
+    //     int[]arr={1,6,3,4};
+    //    boolean result=sortarr(arr, 0);
+    //    System.out.println(result);
+
+    // }
+    
+    
+}
