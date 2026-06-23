84b46936af93fde79b587610e7de1e525adb53b8
rajvendrasingh <sseema7350@gmail.com>
2026-06-23 22:42:31 +0530

    Add files via upload

---
+++ SummerDay17.java
@@ -0,0 +1,30 @@
+public class  SummerDay17 {
+    //print string in reverse
+    //  public static void main(String[] args) {
+    //      String str="abcd";
+    //      String str1=str.substring(0,2);
+    //      System.out.println(str1);
+         
+    //     int leng=str.length();
+    //     for(int i=leng-1; i>=0; i-- ){
+           
+    //        System.out.println(str.charAt(i));
+    //     }
+    // }
+    // reverse string with help of recursion
+//     public static void printRev(String str, int idx){
+//    if(idx==0){
+//     System.out.print(str.charAt(idx));
+//     return;
+//    }
+
+//         System.out.print(str.charAt(idx));
+//         printRev(str, idx-1); 
+//     }
+//     public static void main(String[] args) {
+//         String str="rajvendra";
+//         printRev(str,str.length()-1 );
+//     }
+  
+    
+}
